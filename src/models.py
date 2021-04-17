8a0858e61ce1294156e44c53653f4dbe8509250f
sabjavi <jasabal@hotmail.com>
2021-04-17 22:53:20 +0000

    base de datos instagram.com

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,24 +8,54 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table follower.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_from_id = Column(Integer, nullable=False)
+    user_to_id = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table User.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    username = Column(String(15), nullable=False)
+    firstname = Column(String(15), nullable=False)
+    lastname = Column(String(15), nullable=False)
+    email = Column(String, unique=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    post = relationship ('Post')
+    
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table Media.
+    id = Column(Integer, primary_key=True)
+    types = Column(Enum('Analytics', 'Blogging', 'Business', 'Comunications', 'Copywriting', 'Digital_Marketing', 'Desing', 'Email _Marketing'), nullable=False)
+    url = Column(String(50), nullable=False)
+    post_id = Column(Integer, nullable=False)
+   
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table Post.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    url = Column(String(50), nullable=False)
+    user_id = Column(String, ForeignKey('User.id'))
+    
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table Comment.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(100), nullable=False)
+    author_id = Column(Integer, ForeignKey("author.id"))
+    user = relationship ("User")
+    post_id = Column(Integer, ForeignKey('post.id')) 
+    post = relationship ("Post")
+    media_id = Column(Integer, ForeignKey('media.id')) 
+    media = relationship ("Media")
+    follower_id = Column(Integer, ForeignKey('follower.id')) 
+    Follower = relationship ("Follower")
+    
     def to_dict(self):
         return {}
 
